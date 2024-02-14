c6674bdbcb3e8adf88855f20b4ef368ae9f77a09
zackcis <zakaria.eldahar@gmail.com>
2024-02-15 00:13:33 +0100

    home(first and second sections)

--- src/pages/About/components/firstSection.jsx
+++ src/pages/About/components/firstSection.jsx
@@ -1,15 +1,20 @@
 
 import './firstSection.sass'
 import story from '../../../assets/story.jpg'
+import back from '../../../assets/back.jpg'
 
 export const FirstSectionAbout = () => {
     return (
         <>
-        <div className=''>
-        </div>
+           
+                <div className='relative'>
+                    <img src={back} alt="" />
+                    <h1 className='absolute top-[50%] left-[50%] text-white font-bold text-5xl translate-x-[-50%] translate-y-[-50%]'>CONTACT</h1>
+                </div>
+            
             <div className='flex justify-center items-center w-[100%] h-[70vh] p-5 bg-white gap-4'>
-            <div className='w-[20%]  overflow-hidden ' >
-                    <img className='hover:scale-110  '  src={story} alt="" />
+                <div className='w-[20%]  overflow-hidden ' >
+                    <img className='hover:scale-110  ' src={story} alt="" />
                 </div>
                 <div className='flex flex-col gap-4 justify-center  w-[40%]'>
                     <div className='text-black '>
@@ -18,7 +23,7 @@ export const FirstSectionAbout = () => {
                     <p className='text-gray-400'>Phasellus egestas nisi nisi, lobortis ultricies risus semper nec. Vestibulum pharetra ac ante ut pellentesque. Curabitur fringilla dolor quis lorem accumsan, vitae molestie urna dapibus. Pellentesque porta est ac neque bibendum viverra. Vivamus lobortis magna ut interdum laoreet. Donec gravida lorem elit, quis condimentum ex semper sit amet. Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut fringilla turpis in vehicula vehicula. Pellentesque congue ac orci ut gravida. Aliquam erat volutpat. Donec iaculis lectus a arcu facilisis, eu sodales lectus sagittis. Etiam pellentesque, magna vel dictum rutrum, neque justo eleifend elit, vel tincidunt erat arcu ut sem. Sed rutrum, turpis ut commodo efficitur, quam velit convallis ipsum, et maximus enim ligula ac ligula. Vivamus tristique vulputate ultricies. Sed vitae ultrices orci.</p>
                     <div className='border-l-2 p-5 text-gray-400'>
                         <p>
-                        Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while
+                            Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while
                         </p>
                         <p className='text-black py-4'>- Steve Job's</p>
                     </div>
--- src/pages/Home/components/firstSection.jsx
+++ src/pages/Home/components/firstSection.jsx
@@ -1,12 +1,73 @@
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import './firstSection.sass'
 import { MyContext } from '../../../utils/contextProvider';
+import slide1 from '../../../assets/master-slide-01.jpg'
+import slide2 from '../../../assets/master-slide-02.jpg'
+import slide3 from '../../../assets/master-slide-03.jpg'
+import slide4 from '../../../assets/master-slide-05.jpg'
+import { Carousel } from 'flowbite-react';
+import one from '../../../assets/img1.jpg'
+import second from '../../../assets/img2.jpg'
+import third from '../../../assets/img3.jpg'
+import fourth from '../../../assets/img4.jpg'
+import fifth from '../../../assets/img5.jpg'
+import sixth from '../../../assets/img6.jpg'
+
+
 export const FirstSection = () => {
     const [test, setTest] = useContext(MyContext)
+    const [slide, setSlide] = useState([slide1, slide2, slide3, slide4])
     return (
         <>
-        <h1>test</h1>
+
+            <div className='w-[100%] h-[60vh]'>
+                <Carousel>
+                    {slide.map((slide, index) => (
+                        <div key={slide} className=''>
+                            <img className='relative' src={slide} alt={slide} />
+                            <div className='absolute gap-6 translate-x-[-50] translate-y-[-50] top-[50%] left-[40%] justify-center items-center'>
+                                <p className='font-bold text-2xl text-white'>Only women can wear this</p>
+                                <h1 className='text-white font-bold text-6xl'>NEW ARRIVAL</h1>
+                                <button className='bg-black w-[200px] text-white p-4  text-2xl hover:bg-[#e65540] rounded-full'>Send</button>
+                            </div>
+                        </div>
+                    ))}
+
+                </Carousel>
+
+            </div>
+            <div className='w-[100%] flex justify-center items-center'>
+            <div className='w-[65%] gap-7 flex justify-center items-center p-10  '>
+                <div className='flex flex-col gap-7  justify-center items-center '>
+                    <div className='overflow-hidden'>
+                        <img src={one} alt="" className='hover:scale-110' />
+                    </div>
+                    <div className=' gap-7'>
+                        <img src={fourth} alt="" className='hover:scale-110' />
+                    </div>
+                </div>
+                <div className='flex flex-col gap-7  justify-center items-center '>
+                    <div className='overflow-hidden'>
+                        <img src={second} alt="" className='hover:scale-110' />
+                    </div>
+                    <div className='overflow-hidden'>
+                        <img src={fifth} alt="" className='hover:scale-110' />
+                    </div>
+                </div>
+                <div className='flex flex-col gap-7  justify-center items-center '>
+                    <div className='overflow-hidden'>
+                        <img src={third} alt="" className='hover:scale-110' />
+                    </div>
+                    <div className='overflow-hidden'>
+                        <img src={sixth} alt="" className='hover:scale-110' />
+                    </div>
+                </div>
+
+            </div>
+            </div>
+            
+
         </>
     );
 }
