ce189d7c99b36f8238dd491cea8334a03a8390ea
zackcis <zakaria.eldahar@gmail.com>
2024-02-15 12:34:57 +0100

    third,fourth and fifth sections home

--- src/pages/Home/components/firstSection.jsx
+++ src/pages/Home/components/firstSection.jsx
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import { useContext, useState } from 'react';
 import './firstSection.sass'
 import { MyContext } from '../../../utils/contextProvider';
@@ -13,11 +14,31 @@ import third from '../../../assets/img3.jpg'
 import fourth from '../../../assets/img4.jpg'
 import fifth from '../../../assets/img5.jpg'
 import sixth from '../../../assets/img6.jpg'
+import banner from '../../../assets/banner-08.jpg'
+import ndader from '../../../assets/shop-item-09.jpg'
+import blog1 from '../../../assets/blog-08.jpg'
+import blog2 from '../../../assets/blog-09.jpg'
+import blog3 from '../../../assets/blog-10.jpg'
+
 
 
 export const FirstSection = () => {
-    const [test, setTest] = useContext(MyContext)
-    const [slide, setSlide] = useState([slide1, slide2, slide3, slide4])
+    const [test, setTest, products, setProducts] = useContext(MyContext)
+    const [activeTab, setActiveTab] = useState('New');
+    const [slide, setSlide] = useState([slide1, slide2, slide3, slide4]);
+
+    console.log(products);
+
+    const filteredProducts = products.filter((product, index) => {
+        if (activeTab === 'New') {
+            return product.condition === 'new';
+        } else if (activeTab === 'Old') {
+            return product.condition === 'old';
+        } else if (activeTab === 'Sale') {
+            return product.condition === 'sale';
+        }
+        return true;
+    }).slice(0, 4);;
     return (
         <>
 
@@ -35,39 +56,113 @@ export const FirstSection = () => {
                     ))}
 
                 </Carousel>
-
             </div>
+
+
             <div className='w-[100%] flex justify-center items-center'>
-            <div className='w-[65%] gap-7 flex justify-center items-center p-10  '>
-                <div className='flex flex-col gap-7  justify-center items-center '>
-                    <div className='overflow-hidden'>
-                        <img src={one} alt="" className='hover:scale-110' />
+                <div className='w-[65%] gap-7 flex justify-center items-center p-10  '>
+                    <div className='flex flex-col gap-7  justify-center items-center '>
+                        <div className='overflow-hidden'>
+                            <img src={one} alt="" className='hover:scale-110 relative transition delay-75' />
+                            <button className='bg-white w-[200px] text-black bg-opacity-80 p-4  text-2xl hover:bg-[#e65540]  absolute translate-x-[45%]  translate-y-[-125%]'>Send</button>
+                        </div>
+                        <div className=' gap-7 overflow-hidden'>
+                            <img src={fourth} alt="" className='hover:scale-110 relative transition delay-75' />
+                            <button className='bg-white w-[200px] text-black bg-opacity-80 p-4  text-2xl hover:bg-[#e65540]  absolute translate-x-[45%]  translate-y-[-125%]'>Send</button>
+                        </div>
                     </div>
-                    <div className=' gap-7'>
-                        <img src={fourth} alt="" className='hover:scale-110' />
+                    <div className='flex flex-col gap-7  justify-center items-center '>
+                        <div className='overflow-hidden'>
+                            <img src={second} alt="" className='hover:scale-110 relative transition delay-75' />
+                            <button className='bg-white w-[200px] text-black bg-opacity-80 p-4  text-2xl hover:bg-[#e65540]  absolute translate-x-[45%]  translate-y-[-125%]'>Send</button>
+                        </div>
+                        <div className='overflow-hidden'>
+                            <img src={fifth} alt="" className='hover:scale-110 relative transition delay-75' />
+                            <button className='bg-white w-[200px] text-black bg-opacity-80 p-4  text-2xl hover:bg-[#e65540]  absolute translate-x-[45%]  translate-y-[-125%]'>Send</button>
+                        </div>
                     </div>
-                </div>
-                <div className='flex flex-col gap-7  justify-center items-center '>
-                    <div className='overflow-hidden'>
-                        <img src={second} alt="" className='hover:scale-110' />
-                    </div>
-                    <div className='overflow-hidden'>
-                        <img src={fifth} alt="" className='hover:scale-110' />
-                    </div>
-                </div>
-                <div className='flex flex-col gap-7  justify-center items-center '>
-                    <div className='overflow-hidden'>
-                        <img src={third} alt="" className='hover:scale-110' />
-                    </div>
-                    <div className='overflow-hidden'>
-                        <img src={sixth} alt="" className='hover:scale-110' />
-                    </div>
-                </div>
+                    <div className='flex flex-col gap-7  justify-center items-center '>
+                        <div className='overflow-hidden'>
+                            <img src={third} alt="" className='hover:scale-110 relative transition delay-75' />
+                            <button className='bg-white w-[200px] text-black bg-opacity-80 p-4  text-2xl hover:bg-[#e65540]  absolute translate-x-[45%]  translate-y-[-125%]'>Send</button>
+                        </div>
+                        <div className='overflow-hidden'>
+                            <img src={sixth} alt="" className='hover:scale-110 relative transition delay-75' />
+                            <button className='bg-white w-[200px] text-black bg-opacity-80 p-4  text-2xl hover:bg-[#e65540]  absolute translate-x-[45%]  translate-y-[-125%]'>Send</button>
 
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-            
 
+            <div className='w-[100% ] flex flex-col justify-center items-center'>
+                <div className=' flex justify-center w-[30%] items-center font-bold text-4xl gap-10 mt-7 border-gray-700 border-2'>
+                    <button onClick={() => setActiveTab('New')}>New</button>
+                    <button onClick={() => setActiveTab('Old')}>Old</button>
+                    <button onClick={() => setActiveTab('Sale')}>Sale</button>
+                </div>
+                <div className='w-[100%] p-24 flex gap-5 justify-center items-center '>
+                    {filteredProducts.map(product => (
+                        <div className='w-[20%]' key={product.name}>
+                            <div className=' overflow-hidden'>
+                                <img className=' hover:scale-110 relative transition delay-75' src={product.image} alt={product.name} />
+                            </div>
+                            <h3>{product.name}</h3>
+                            <p>Price: ${product.price}</p>
+                            <p>Condition: {product.condition}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className='bg-[#f2f2f2] w-[100%] flex p-10 gap-7 justify-center items-center '>
+                <div className='w-[30%] overflow-hidden'>
+                    <img className='relative hover:scale-110 transition delay-75' src={banner} alt="" />
+                    <div className='flex flex-col gap-1 text-white text-center absolute translate-x-24 translate-y-[-200%]'>
+                        <p className='font-bold text-4xl'>The Beauty</p>
+                        <h1 className='font-bold text-7xl'>LOOKBOOK</h1>
+                        <p>View COLLECTION</p>
+                    </div>
+                </div>
+                <div className='w-[30%] overflow-hidden '>
+                    <img className='relative transition delay-75 hover:scale-110' src={ndader} alt="" />
+                    <div className='absolute translate-x-44 translate-y-[-350%] text-center text-black'>
+                        <p>Boxy2 T-Shirt with Roll Sleeve</p>
+                        <p>£20.00</p>
+                    </div>
+                </div>
+            </div>
+            <div className='w-[100%] bg-white flex flex-col p-11'>
+                <h1 className='text-4xl font-bold text-center py-16'>OUR Blog</h1>
+                <div className='w-[100%] flex justify-center items-center gap-7'>
+                    <div className='w-[23%] flex flex-col  justify-center items-start gap-4  '>
+                        <div className='overflow-hidden'>
+                            <img className='transition delay-75 hover:scale-110' src={blog1} alt="" />
+                        </div>
+                        <h2 className='text-start text-black cursor-pointer hover:text-[#e65540] transition delay-75 '>Black Friday Guide: Best Sales & Discount Codes</h2>
+                        <p className='text-gray-500'>by fashe-theme Admin on Dec 28,2017
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed turpis sed lorem dignissim vulputate nec cursus ante. Nunc sit...</p>
+                    </div>
+                    <div className='w-[23%] flex flex-col  justify-center items-start gap-4  '>
+                        <div className='overflow-hidden'>
+                            <img className='transition delay-75 hover:scale-110' src={blog2} alt="" />
+                        </div>
+                        <h2 className='text-start text-black cursor-pointer hover:text-[#e65540] transition delay-75 '>Black Friday Guide: Best Sales & Discount Codes</h2>
+                        <p className='text-gray-500'>by fashe-theme Admin on Dec 28,2017
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed turpis sed lorem dignissim vulputate nec cursus ante. Nunc sit...</p>
+                    </div>
+                    <div className='w-[23%] flex flex-col  justify-center items-start gap-4  '>
+                        <div className='overflow-hidden'>
+                            <img className='transition delay-75 hover:scale-110' src={blog3} alt="" />
+                        </div>
+                        <h2 className='text-start text-black cursor-pointer hover:text-[#e65540] transition delay-75 '>Black Friday Guide: Best Sales & Discount Codes</h2>
+                        <p className='text-gray-500'>by fashe-theme Admin on Dec 28,2017
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed turpis sed lorem dignissim vulputate nec cursus ante. Nunc sit...</p>
+                    </div>
+                </div>
+            </div>
+<div className='w-[100%] flex justify-center items-center '>
+    <h1 className='font-bold text-5xl p-28'>@ FOLLOW INSTAGRAM</h1>
+</div>
         </>
     );
 }
--- src/utils/contextProvider.jsx
+++ src/utils/contextProvider.jsx
@@ -1,11 +1,109 @@
+import img4 from '../assets/item-03.jpg';  
+import img5 from '../assets/item-04.jpg';
+import img6 from '../assets/item-10.jpg'; 
+import img7 from '../assets/item-11.jpg';  
+import img8 from '../assets/item-12.jpg';
+import img9 from '../assets/item-13.jpg'; 
+import img10 from  '../assets/item-14.jpg';
+import img11 from '../assets/item-15.jpg'; 
+import img12 from '../assets/item-16.jpg';
 
 import React, { createContext, useState } from 'react'
 export const MyContext = createContext()
 export const MyProvider = ({ children }) => {
     const [test, setTest] = useState('Hello Test')
+
+
+    const [products, setProducts] = useState([
+        {
+            name: "Kylie Cosmetics Lip Gloss",
+            image: img4,
+            description: "Shine bright with Kylie's signature lip gloss.",
+            price: "19.99",
+            gender: "women",
+            size: "One Size",
+            condition: "new",
+        },
+        {
+            name: "KKW Beauty Highlighter",
+            image: img5,
+            description: "Illuminate your features with Kim's top-rated highlighter.",
+            price: "34.99",
+            gender: "women",
+            size: "One Size",
+            condition: "new",
+        },
+        {
+            name: "Kylie Skin Moisturizer",
+            image: img6,
+            description: "Keep your skin hydrated and glowing with Kylie's moisturizer.",
+            price: "25.00",
+            gender: "women",
+            size: "One Size",
+            condition: "old",
+        },
+        {
+            name: "KKW Fragrance",
+            image: img7,
+            description: "Indulge in Kim's signature fragrance for a luxurious scent.",
+            price: "49.99",
+            gender: "women",
+            size: "One Size",
+            condition: "new",
+        },
+        {
+            name: "Kylie Eyeliner Pen",
+            image: img8,
+            description: "Achieve the perfect winged eyeliner with Kylie's easy-to-use pen.",
+            price: "14.99",
+            gender: "women",
+            size: "One Size",
+            condition: "old",
+        },
+        {
+            name: "KKW Beauty Lip Liner",
+            image: img9,
+            description: "Define and enhance your lips with Kim's lip liner.",
+            price: "17.99",
+            gender: "women",
+            size: "One Size",
+            condition: "sale",
+        },
+        {
+            name: "Kylie Cosmetics Eyebrow Kit",
+            image: img10,
+            description: "Shape and fill your eyebrows with Kylie's eyebrow kit.",
+            price: "22.99",
+            gender: "women",
+            size: "One Size",
+            condition: "old",
+        },
+        {
+            name: "KKW Beauty Setting Powder",
+            image: img11,
+            description: "Set your makeup for all-day wear with Kim's setting powder.",
+            price: "28.99",
+            gender: "women",
+            size: "One Size",
+            condition: "sale",
+        },
+        {
+            name: "Kylie Skin Face Mask",
+            image: img12,
+            description: "Revitalize your skin with Kylie's luxurious face mask.",
+            price: "32.00",
+            gender: "women",
+            size: "One Size",
+            condition: "new",
+        },
+
+        // Add more products as needed
+    ]);
+
     return (
         <>
-            <MyContext.Provider value={ [test, setTest] } >
+
+            <MyContext.Provider value={[test, setTest,products, setProducts]} >
                 {children}
             </MyContext.Provider>
         </>
